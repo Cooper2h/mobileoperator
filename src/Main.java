76099d942c045dec8415d9c98f9091134d30c64a
Cooper <step.stepstep@yandex.ru>
2024-09-27 15:09:01 +0300

    исправлено

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,17 @@ public class Main {
         int initialBalance = 100; //начальный счет
         int amount = 1100; // сумма пополнения
 
-        // бонус
+        // бонус за каждые 100 рублей
         int bonus = 0;
         if (amount > 1000) {
-            bonus = (amount - 1000) / 10;
+            bonus = amount / 100;
 
         }
 
         //Итоговый баланс
         int balance = initialBalance + amount + bonus;
 
-//Результат
+        //Результат
         System.out.println("Итоговый баланс: " + balance + " рублей");
         System.out.println("Бонус: " + bonus + " рублей");
     }
